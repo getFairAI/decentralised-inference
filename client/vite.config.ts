23d2a69a4c2b625c73e29e0877a239da91c51a93
l-silvestre <lcssilvestre98@gmail.com>
2023-03-05 16:29:18 +0000

    chore: set up lint and prettier for react client app project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), nodePolyfills({ protocolImports: true })],
+  plugins: [react(), tsconfigPaths(), nodePolyfills({ protocolImports: true }), eslint()],
   build: {
     rollupOptions: {
       external: '**/_*.tsx'
     }
   }
-})
+});
