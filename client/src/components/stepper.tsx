6ef77fb03e41d9ba5ed02d0f240bbeecf860d3d2
l-silvestre <lcssilvestre98@gmail.com>
2023-03-06 15:12:45 +0000

    fix: fix stepper file format after rebase

--- client/src/components/stepper.tsx
+++ client/src/components/stepper.tsx
@@ -17,7 +17,7 @@ import {
   TextField,
   Typography,
   List,
-  ListItem
+  ListItem,
 } from '@mui/material';
 import { useState } from 'react';
 import PaymentIcon from '@mui/icons-material/Payment';
@@ -344,91 +344,158 @@ export const CustomStepper = (props: {
             preview='preview'
             previewOptions={{
               rehypePlugins: [[rehypeSanitize]],
-            }} hideToolbar={true} fullscreen={false} value={'### Requirements\nExample Markdown'}></MDEditor>
-            <Box>
-              <FormControl variant="outlined" fullWidth>
-                <TextField
-                  multiline
-                  disabled
-                  minRows={1}
-                  value={'Model name'}
-                  InputProps={{
-                    startAdornment: <InputAdornment position='start'><IconButton aria-label="download" /* onClick={handleDownload} */ ><DownloadIcon /></IconButton></InputAdornment>,
-                    endAdornment: <InputAdornment position="start">{'15MB'}</InputAdornment>,
-                    readOnly: true 
-                  }}
-                />
-              </FormControl>
-            </Box>
-            <Box display={'flex'} justifyContent={'space-between'}>
-              <Button variant='outlined' onClick={handleBack}>Back</Button>
-              <Button variant='contained' onClick={handleNext}>Next</Button>
-            </Box>
-          </React.Fragment>
-        ) : (        
-          <React.Fragment>
-            <Typography variant='h4' textAlign={'Justify'}>
-              <b>Rules, Terms, and Conditions of the App</b>
-            </Typography>
-            <Typography variant='h5' textAlign={'Justify'}>
-              Terminology:
-            </Typography>
-            <List
-              sx = {{
-                listStyleType: 'disc',
-                pl: 2,
-                lineHeight: 0.5,
-                '& .MuiListItem-root': {
-                  display: 'list-item',
-                },
-              }}>
-              <ListItem>
+            }}
+            hideToolbar={true}
+            fullscreen={false}
+            value={'### Requirements\nExample Markdown'}
+          ></MDEditor>
+          <Box>
+            <FormControl variant='outlined' fullWidth>
+              <TextField
+                multiline
+                disabled
+                minRows={1}
+                value={'Model name'}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position='start'>
+                      <IconButton aria-label='download' /* onClick={handleDownload} */>
+                        <DownloadIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                  endAdornment: <InputAdornment position='start'>{'15MB'}</InputAdornment>,
+                  readOnly: true,
+                }}
+              />
+            </FormControl>
+          </Box>
+          <Box display={'flex'} justifyContent={'space-between'}>
+            <Button variant='outlined' onClick={handleBack}>
+              Back
+            </Button>
+            <Button variant='contained' onClick={handleNext}>
+              Next
+            </Button>
+          </Box>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Typography variant='h4' textAlign={'justify'}>
+            <b>Rules, Terms, and Conditions of the App</b>
+          </Typography>
+          <Typography variant='h5' textAlign={'justify'}>
+            Terminology:
+          </Typography>
+          <List
+            sx={{
+              listStyleType: 'disc',
+              pl: 2,
+              lineHeight: 0.5,
+              '& .MuiListItem-root': {
+                display: 'list-item',
+              },
+            }}
+          >
+            <ListItem>
               A Creator is anyone who inserts trained models on the Fair Protocol marketplace;
-              </ListItem>
-              <ListItem>
+            </ListItem>
+            <ListItem>
               A User is anyone who requests inference tasks on models inserted by Creators;
-              </ListItem>
-              <ListItem>
-            An Operator is anyone who responds to those inferences requests made by Users, running the specified model inference and returning it.
-              </ListItem>
-            </List>  
-            <Typography variant='body1' textAlign={'Justify'}>
-              All the communication between participants in this network is done through Arweave. This means that a Creator inserts trained models on Arweave, so users can request inferences on those models through Arweave, and Operators can send responses on Arweave. When anything is written on Arweave, it&apos;s stored forever, due to the particularities of that blockchain. {'\n'}
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              A Creator defines all the rules needed to install a model, as well as the script that the Operators should run, and the rules for getting such a script to work. A Creator then sends all this data to Arweave, and for his model to be listed in the marketplace he must pay a fee to the marketplace of 0.1 AR. The fee is intended to filter out people who want to put junk on the marketplace.
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              To become an Operator you have to install a template inserted in Arweave by a Creator, following the rules defined by that Creator. These rules should result in a script that will run in some kind of infinite loop, waiting for some inference request to be made by a User. <b>It is the Operator&apos;s responsibility to verify that a Creator&apos;s code and rules make sense and are legit. The instructions may be a scam and the code may contain malware. We highly advise you to use a PC other than your own to perform the inferences.</b>
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              After a successful installation, an Operator should make a transaction to Arweave saying that it started business on that specific model, in order to inform the marketplace. This transaction will have an extra cost of 0.05 AR, sent to the marketplace, to filter out some potential bad actors. Operators charge a fee for performing inferences to Users. This fee is specified by themselves in the start business transaction.
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              <b>If an Operator starts business, but doen&apos;t return any inference required by a User within <b>X blocks</b>, it will be removed from the marketplace as a viable option to perform inference, and will only be able to perform inference again if it realizes a new transaction to open a business.</b> <b>To do: decide about blocks/time until penalization</b>
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              <b>Statistics are especially important for Operators, as this is what Users will rely on when choosing someone to perform inference. To obtain the best possible statistics, an Operator must return as many inferences as possible without failing to return any.</b>
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              Operators can decide to terminate business whenever they want. To do so, they should send a new transaction, specifying this business termination. This transaction will be free of costs, and the advantage of doing it will be that no penalization will be executed by the marketplace to the Operator, and will have best stats. They can start business again later on. <b>To do: specify rules for this in the app</b>
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              To reward the train of models executed by Creators as well as the fees paid by those Creators when submitting models to Arweave, the marketplace ensures that every time a User requests an inference on a specified model to an Operator, it pays an extra 5% fee to the Operator, so the Operator can reward afterwards the Creator of that model.
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              The Fair Protocol marketplace will ensure that all participants follow all the rules specified, charging another 5% fee for that service to Users when they request inferences. Those 5% will be paid to Operators, that in turn will pay to the marketplace. <b>If any of those extra fees are not paid by the Operator, the marketplace won&apos;t show the results of the inference to the User. If was the Operator who didn&apos;t had paid back, he will be penalized by the marketplace, by not being shown as a valid option to Users.</b> <b>To do: detail more these rules</b>
-            </Typography>
-            <Typography variant='body1' textAlign={'Justify'}>
-              <b>By clicking next, you accept all these rules, terms, and conditions specified above.</b>
-            </Typography>
-            <Box display={'flex'} justifyContent={'flex-end'}>
-              {/* <Button variant='contained' onClick={handleBack}>Back</Button> */}
-              <Button variant='contained' onClick={handleNext}>Next</Button>
-            </Box>
-          </React.Fragment>
-        )
-      }
+            </ListItem>
+            <ListItem>
+              An Operator is anyone who responds to those inferences requests made by Users, running
+              the specified model inference and returning it.
+            </ListItem>
+          </List>
+          <Typography variant='body1' textAlign={'justify'}>
+            All the communication between participants in this network is done through Arweave. This
+            means that a Creator inserts trained models on Arweave, so users can request inferences
+            on those models through Arweave, and Operators can send responses on Arweave. When
+            anything is written on Arweave, it&apos;s stored forever, due to the particularities of
+            that blockchain. {'\n'}
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            A Creator defines all the rules needed to install a model, as well as the script that
+            the Operators should run, and the rules for getting such a script to work. A Creator
+            then sends all this data to Arweave, and for his model to be listed in the marketplace
+            he must pay a fee to the marketplace of 0.1 AR. The fee is intended to filter out people
+            who want to put junk on the marketplace.
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            To become an Operator you have to install a template inserted in Arweave by a Creator,
+            following the rules defined by that Creator. These rules should result in a script that
+            will run in some kind of infinite loop, waiting for some inference request to be made by
+            a User.{' '}
+            <b>
+              It is the Operator&apos;s responsibility to verify that a Creator&apos;s code and
+              rules make sense and are legit. The instructions may be a scam and the code may
+              contain malware. We highly advise you to use a PC other than your own to perform the
+              inferences.
+            </b>
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            After a successful installation, an Operator should make a transaction to Arweave saying
+            that it started business on that specific model, in order to inform the marketplace.
+            This transaction will have an extra cost of 0.05 AR, sent to the marketplace, to filter
+            out some potential bad actors. Operators charge a fee for performing inferences to
+            Users. This fee is specified by themselves in the start business transaction.
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            <b>
+              If an Operator starts business, but doen&apos;t return any inference required by a
+              User within <b>X blocks</b>, it will be removed from the marketplace as a viable
+              option to perform inference, and will only be able to perform inference again if it
+              realizes a new transaction to open a business.
+            </b>{' '}
+            <b>To do: decide about blocks/time until penalization</b>
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            <b>
+              Statistics are especially important for Operators, as this is what Users will rely on
+              when choosing someone to perform inference. To obtain the best possible statistics, an
+              Operator must return as many inferences as possible without failing to return any.
+            </b>
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            Operators can decide to terminate business whenever they want. To do so, they should
+            send a new transaction, specifying this business termination. This transaction will be
+            free of costs, and the advantage of doing it will be that no penalization will be
+            executed by the marketplace to the Operator, and will have best stats. They can start
+            business again later on. <b>To do: specify rules for this in the app</b>
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            To reward the train of models executed by Creators as well as the fees paid by those
+            Creators when submitting models to Arweave, the marketplace ensures that every time a
+            User requests an inference on a specified model to an Operator, it pays an extra 5% fee
+            to the Operator, so the Operator can reward afterwards the Creator of that model.
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            The Fair Protocol marketplace will ensure that all participants follow all the rules
+            specified, charging another 5% fee for that service to Users when they request
+            inferences. Those 5% will be paid to Operators, that in turn will pay to the
+            marketplace.{' '}
+            <b>
+              If any of those extra fees are not paid by the Operator, the marketplace won&apos;t
+              show the results of the inference to the User. If was the Operator who didn&apos;t had
+              paid back, he will be penalized by the marketplace, by not being shown as a valid
+              option to Users.
+            </b>{' '}
+            <b>To do: detail more these rules</b>
+          </Typography>
+          <Typography variant='body1' textAlign={'justify'}>
+            <b>
+              By clicking next, you accept all these rules, terms, and conditions specified above.
+            </b>
+          </Typography>
+          <Box display={'flex'} justifyContent={'flex-end'}>
+            {/* <Button variant='contained' onClick={handleBack}>Back</Button> */}
+            <Button variant='contained' onClick={handleNext}>
+              Next
+            </Button>
+          </Box>
+        </React.Fragment>
+      )}
     </Stack>
   );
 };
