a5fb9d8be132f89463716eb1e41213b15bfdeef7
l-silvestre <lcssilvestre98@gmail.com>
2023-03-06 16:35:18 +0000

    chore: install and setup vitest and necessary dependencies

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,3 @@
-// / <reference types="vite/client" />
-// / <reference types="arconnect" />
+/// <reference types="vite/client" />
+/// <reference types="arconnect" />
+/// <reference types="vitest" />
