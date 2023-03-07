14334dc5352487fc9872b00bc1d849ca3ba45ff9
l-silvestre <lcssilvestre98@gmail.com>
2023-03-07 16:59:55 +0000

    fix: fix format errors

--- client/src/components/progress.tsx
+++ client/src/components/progress.tsx
@@ -17,9 +17,11 @@ const CustomProgress = (props: LinearProgressProps & { value: number }) => {
           <LinearProgress variant='determinate' /* {...props} */ value={progress} />
         </Box>
         <Box sx={{ minWidth: 35 }}>
-          <Typography variant='body2' color='text.secondary' data-testid='progress-bar-text'>{`${Math.round(
-            progress,
-          )}%`}</Typography>
+          <Typography
+            variant='body2'
+            color='text.secondary'
+            data-testid='progress-bar-text'
+          >{`${Math.round(progress)}%`}</Typography>
         </Box>
       </Box>
     </Box>
--- client/src/tests/components/progress.test.tsx
+++ client/src/tests/components/progress.test.tsx
@@ -1,15 +1,15 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CustomProgress from '@/components/progress';
 
 describe('components/progress.tsx', () => {
   it('should display progress bar with percetage 10%', async () => {
-    render(<CustomProgress value={10}/>);
+    render(<CustomProgress value={10} />);
     expect(await screen.queryByTestId('progress-bar-text')?.textContent).toEqual('10%');
   });
 
   it('should update value', async () => {
     // value = 40;
-    const { rerender } = render(<CustomProgress value={10}/>);
+    const { rerender } = render(<CustomProgress value={10} />);
     expect(await screen.queryByTestId('progress-bar-text')?.textContent).toEqual('10%');
 
     rerender(<CustomProgress value={60} />);
