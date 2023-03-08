4e4e9b8702d73f7ccc4140e7f64655a8fff3b37f
l-silvestre <lcssilvestre98@gmail.com>
2023-03-08 17:13:03 +0000

    fix: fix small layout issues

--- client/src/pages/operator/detail.tsx
+++ client/src/pages/operator/detail.tsx
@@ -128,113 +128,115 @@ const OperatorDetails = () => {
                 />
               </Box>
               <Divider textAlign='left'>History</Divider>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell variant='head'>TxID</TableCell>
-                      <TableCell variant='head'>Origin</TableCell>
-                      <TableCell variant='head'>Destination</TableCell>
-                      <TableCell variant='head'>Type</TableCell>
-                      <TableCell variant='head'>Date&nbsp;</TableCell>
-                      <TableCell variant='head' align='right'>
-                        Network Fee&nbsp;(Ar)
-                      </TableCell>
-                      <TableCell variant='head' align='right'>
-                        Application Fee&nbsp;(Ar)
-                      </TableCell>
-                      <TableCell variant='head' align='right'>
-                        Model Tx
-                      </TableCell>
-                      <TableCell variant='head' align='right'>
-                        Operation
-                      </TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {loading ? (
+              <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                <TableContainer>
+                  <Table sx={{ minWidth: 650, maxHeight: 700 }} aria-label='simple table' stickyHeader>
+                    <TableHead>
                       <TableRow>
-                        <TableCell colSpan={9} align='center'>
-                          <Typography>Loading...</Typography>
+                        <TableCell variant='head'>TxID</TableCell>
+                        <TableCell variant='head'>Origin</TableCell>
+                        <TableCell variant='head'>Destination</TableCell>
+                        <TableCell variant='head'>Type</TableCell>
+                        <TableCell variant='head'>Date&nbsp;</TableCell>
+                        <TableCell variant='head' align='right'>
+                          Network Fee&nbsp;(Ar)
+                        </TableCell>
+                        <TableCell variant='head' align='right'>
+                          Application Fee&nbsp;(Ar)
+                        </TableCell>
+                        <TableCell variant='head' align='right'>
+                          Model Tx
+                        </TableCell>
+                        <TableCell variant='head' align='right'>
+                          Operation
                         </TableCell>
                       </TableRow>
-                    ) : (
-                      rows.map((row: Row, idx: number) => (
-                        <TableRow
-                          key={idx}
-                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                          <TableCell scope='row'>
-                            <Tooltip title={row.txid}>
-                              <Typography display={'flex'}>
-                                {row.txid.slice(0, 5)}...{row.txid.slice(-2)}
-                                <IconButton
-                                  size='small'
-                                  onClick={() => {
-                                    navigator.clipboard.writeText(row.txid);
-                                  }}
-                                >
-                                  <ContentCopyIcon fontSize='inherit' />
-                                </IconButton>
-                              </Typography>
-                            </Tooltip>
+                    </TableHead>
+                    <TableBody>
+                      {loading ? (
+                        <TableRow>
+                          <TableCell colSpan={9} align='center'>
+                            <Typography>Loading...</Typography>
                           </TableCell>
-                          <TableCell align='right'>
-                            <Tooltip title={row.origin}>
-                              <Typography display={'flex'}>
-                                {row.origin.slice(0, 5)}...{row.origin.slice(-2)}
-                                <IconButton
-                                  size='small'
-                                  onClick={() => {
-                                    navigator.clipboard.writeText(row.origin);
-                                  }}
-                                >
-                                  <ContentCopyIcon fontSize='inherit' />
-                                </IconButton>
-                              </Typography>
-                            </Tooltip>
-                          </TableCell>
-                          <TableCell align='right'>
-                            <Tooltip title={row.destination}>
-                              <Typography display={'flex'}>
-                                {row.destination.slice(0, 5)}...{row.destination.slice(-2)}
-                                <IconButton
-                                  size='small'
-                                  onClick={() => {
-                                    navigator.clipboard.writeText(row.destination);
-                                  }}
-                                >
-                                  <ContentCopyIcon fontSize='inherit' />
-                                </IconButton>
-                              </Typography>
-                            </Tooltip>
-                          </TableCell>
-                          <TableCell align='right'>{row.type}</TableCell>
-                          <TableCell align='right'>{row.date}</TableCell>
-                          <TableCell align='right'>{row.networkFee}</TableCell>
-                          <TableCell align='right'>{row.appFee}</TableCell>
-                          <TableCell align='right'>
-                            <Tooltip title={row.modelTx}>
-                              <Typography display={'flex'}>
-                                {row.modelTx.slice(0, 5)}...{row.modelTx.slice(-2)}
-                                <IconButton
-                                  size='small'
-                                  onClick={() => {
-                                    navigator.clipboard.writeText(row.modelTx);
-                                  }}
-                                >
-                                  <ContentCopyIcon fontSize='inherit' />
-                                </IconButton>
-                              </Typography>
-                            </Tooltip>
-                          </TableCell>
-                          <TableCell align='right'>{row.operation}</TableCell>
                         </TableRow>
-                      ))
-                    )}
-                  </TableBody>
-                </Table>
-              </TableContainer>
+                      ) : (
+                        rows.map((row: Row, idx: number) => (
+                          <TableRow
+                            key={idx}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                            <TableCell scope='row'>
+                              <Tooltip title={row.txid}>
+                                <Typography display={'flex'}>
+                                  {row.txid.slice(0, 5)}...{row.txid.slice(-2)}
+                                  <IconButton
+                                    size='small'
+                                    onClick={() => {
+                                      navigator.clipboard.writeText(row.txid);
+                                    }}
+                                  >
+                                    <ContentCopyIcon fontSize='inherit' />
+                                  </IconButton>
+                                </Typography>
+                              </Tooltip>
+                            </TableCell>
+                            <TableCell align='right'>
+                              <Tooltip title={row.origin}>
+                                <Typography display={'flex'}>
+                                  {row.origin.slice(0, 5)}...{row.origin.slice(-2)}
+                                  <IconButton
+                                    size='small'
+                                    onClick={() => {
+                                      navigator.clipboard.writeText(row.origin);
+                                    }}
+                                  >
+                                    <ContentCopyIcon fontSize='inherit' />
+                                  </IconButton>
+                                </Typography>
+                              </Tooltip>
+                            </TableCell>
+                            <TableCell align='right'>
+                              <Tooltip title={row.destination}>
+                                <Typography display={'flex'}>
+                                  {row.destination.slice(0, 5)}...{row.destination.slice(-2)}
+                                  <IconButton
+                                    size='small'
+                                    onClick={() => {
+                                      navigator.clipboard.writeText(row.destination);
+                                    }}
+                                  >
+                                    <ContentCopyIcon fontSize='inherit' />
+                                  </IconButton>
+                                </Typography>
+                              </Tooltip>
+                            </TableCell>
+                            <TableCell align='right'>{row.type}</TableCell>
+                            <TableCell align='right'>{row.date}</TableCell>
+                            <TableCell align='right'>{row.networkFee}</TableCell>
+                            <TableCell align='right'>{row.appFee}</TableCell>
+                            <TableCell align='right'>
+                              <Tooltip title={row.modelTx}>
+                                <Typography display={'flex'}>
+                                  {row.modelTx.slice(0, 5)}...{row.modelTx.slice(-2)}
+                                  <IconButton
+                                    size='small'
+                                    onClick={() => {
+                                      navigator.clipboard.writeText(row.modelTx);
+                                    }}
+                                  >
+                                    <ContentCopyIcon fontSize='inherit' />
+                                  </IconButton>
+                                </Typography>
+                              </Tooltip>
+                            </TableCell>
+                            <TableCell align='right'>{row.operation}</TableCell>
+                          </TableRow>
+                        ))
+                      )}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>
               {/* <TablePagination
               rowsPerPageOptions={[5, 10, 25]}
               component="div"
