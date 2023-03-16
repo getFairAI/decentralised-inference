3e51be3ed3e69987e0bc01bc4ef700e912ebc14e
l-silvestre <lcssilvestre98@gmail.com>
2023-03-16 22:47:56 +0000

    fix: fix files formatting

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -4,12 +4,16 @@ import { ReactElement, useState } from 'react';
 import Navbar from './navbar';
 
 export default function Layout({ children }: { children: ReactElement }) {
-  const [ showBanner, setShowBanner ] = useState(true);
-  
+  const [showBanner, setShowBanner] = useState(true);
+
   return (
     <>
-      <Navbar showBanner={showBanner} setShowBanner={setShowBanner}/>
-      <Container disableGutters sx={{ width: '100%', height: showBanner ? 'calc(100% - 94px)' : 'calc(100% - 64px)' }} maxWidth={false} >
+      <Navbar showBanner={showBanner} setShowBanner={setShowBanner} />
+      <Container
+        disableGutters
+        sx={{ width: '100%', height: showBanner ? 'calc(100% - 94px)' : 'calc(100% - 64px)' }}
+        maxWidth={false}
+      >
         <Box height='100%'>
           <main style={{ height: '100%' }}>{children}</main>
         </Box>
