08189ad846453a7e975da53da336595ac8be1863
l-silvestre <lcssilvestre98@gmail.com>
2023-03-26 08:09:15 +0100

    feat: implement choose operator screen design

--- client/src/pages/model/detail.tsx
+++ client/src/pages/model/detail.tsx
@@ -1,22 +1,16 @@
 import {
-  Avatar,
   Button,
-  Card,
-  CardContent,
-  Container,
-  Divider,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  SvgIcon,
-  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Icon,
+  IconButton,
   Typography,
 } from '@mui/material';
 import { Box } from '@mui/system';
 import BasicTable from '@/components/basic-table';
-import Stamp from '@/components/stamp';
-import { useLocation, useRouteLoaderData } from 'react-router-dom';
+import { useLocation, useNavigate, useRouteLoaderData } from 'react-router-dom';
 import { NetworkStatus, useQuery } from '@apollo/client';
 import { QUERY_REGISTERED_OPERATORS } from '@/queries/graphql';
 import {
@@ -26,22 +20,27 @@ import {
   REGISTER_OPERATION_TAG,
 } from '@/constants';
 import { IEdge, ITag } from '@/interfaces/arweave';
-import { ChangeEvent, useContext, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 import { useSnackbar } from 'notistack';
 import arweave from '@/utils/arweave';
-import { WalletContext } from '@/context/wallet';
-import { NumericFormat } from 'react-number-format';
+import { toSvg } from 'jdenticon';
 
 const Detail = () => {
   const updatedFee = useRouteLoaderData('model') as string;
   const { state } = useLocation();
+  const navigate = useNavigate();
   const [operatorsData, setOperatorsData] = useState<IEdge[]>([]);
   const [hasNextPage, setHasNextPage] = useState(false);
   const [feeValue, setFeeValue] = useState(0);
-  const [feeDirty, setFeeDirty] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
-  const { currentAddress: owner } = useContext(WalletContext);
+  const [ showOperators, setShowOperators ] = useState(false);
   const elementsPerPage = 5;
+
+  const imgUrl = useMemo(() => {
+    const img = toSvg(state.node.id, 100);
+    const svg = new Blob([img], { type: 'image/svg+xml' });
+    return URL.createObjectURL(svg);
+  }, [ state ]);
 
   const tags = [
     ...DEFAULT_TAGS,
@@ -85,51 +84,6 @@ const Detail = () => {
     }
   }, [state]);
 
-  const handleFeeChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const val = parseFloat(event.target.value);
-    setFeeValue(val);
-    setFeeDirty(true);
-  };
-
-  const updateFee = async () => {
-    try {
-      const tx = await arweave.createTransaction({
-        quantity: arweave.ar.arToWinston('0'),
-        target: MARKETPLACE_ADDRESS,
-      });
-      tx.addTag('App-Name', 'Fair Protocol');
-      tx.addTag('App-Version', APP_VERSION);
-      tx.addTag('Operation-Name', 'Model Fee Update');
-      tx.addTag(
-        'Model-Transaction',
-        state.node.tags.find((el: ITag) => el.name === 'Model-Transaction')?.value,
-      );
-      tx.addTag('Model-Fee', arweave.ar.arToWinston(`${feeValue}`));
-      tx.addTag('Unix-Time', (Date.now() / 1000).toString());
-      await arweave.transactions.sign(tx);
-      const payRes = await arweave.transactions.post(tx);
-      if (payRes.status === 200) {
-        enqueueSnackbar(
-          <>
-            Updated Model Fee
-            <br></br>
-            <a href={`https://viewblock.io/arweave/tx/${tx.id}`} target={'_blank'} rel='noreferrer'>
-              <u>View Transaction in Explorer</u>
-            </a>
-          </>,
-          {
-            variant: 'success',
-          },
-        );
-        setFeeDirty(false);
-      } else {
-        enqueueSnackbar(payRes.statusText, { variant: 'error' });
-      }
-    } catch (err) {
-      enqueueSnackbar('Something Went Wrong', { variant: 'error' });
-    }
-  };
-
   useEffect(() => {
     // check has paid correct registration fee
     if (queryData && networkStatus === NetworkStatus.ready) {
@@ -145,98 +99,142 @@ const Detail = () => {
     }
   }, [queryData]);
 
-  return (
-    <Container>
-      <Box sx={{ margin: '8px' }}>
-        <Card>
-          <CardContent>
-            <Box display={'flex'} justifyContent={'space-evenly'} marginBottom={'16px'}>
-              <Box display={'flex'} flexDirection={'column'} justifyContent={'space-between'}>
-                <Avatar
-                  sx={{ width: '180px', height: '180px' }}
-                  src={state?.node?.tags?.find((el: ITag) => el.name === 'AvatarUrl')?.value}
-                />
-                {owner === state.node.owner.address ? (
-                  <Button variant='outlined' disabled={!feeDirty} onClick={updateFee}>
-                    Update
-                  </Button>
-                ) : (
-                  <Button
-                    variant='outlined'
-                    startIcon={
-                      <SvgIcon>
-                        <Stamp />
-                      </SvgIcon>
-                    }
-                  >
-                    Stamp
-                  </Button>
-                )}
-              </Box>
-              <Box>
-                <TextField
-                  label='Name'
-                  variant='outlined'
-                  value={state?.node?.tags?.find((el: ITag) => el.name === 'Model-Name')?.value}
-                  fullWidth
-                  inputProps={{ readOnly: true }}
-                  sx={{ width: '70%' }}
-                />
-                {owner === state.node.owner.address ? (
-                  <NumericFormat
-                    value={feeValue}
-                    onChange={handleFeeChange}
-                    customInput={TextField}
-                    decimalScale={4}
-                    label='Fee'
-                    variant='outlined'
-                    decimalSeparator={'.'}
-                    sx={{ width: '25%' }}
-                  />
-                ) : (
-                  <NumericFormat
-                    value={feeValue}
-                    onChange={handleFeeChange}
-                    customInput={TextField}
-                    decimalScale={4}
-                    label='Fee'
-                    variant='outlined'
-                    decimalSeparator={'.'}
-                    sx={{ width: '25%' }}
-                    inputProps={{ readOnly: true }}
-                  />
-                )}
+  const handleClose = () => {
+    navigate('/');
+  };
 
-                <FormControl fullWidth margin='normal'>
-                  <InputLabel>Category</InputLabel>
-                  <Select
-                    data-testid={'category'}
-                    value={state?.node?.tags?.find((el: ITag) => el.name === 'Category')?.value}
-                    label='Category'
-                    inputProps={{ readOnly: true }}
-                  >
-                    <MenuItem value={'text'}>Text</MenuItem>
-                    <MenuItem value={'audio'}>Audio</MenuItem>
-                    <MenuItem value={'video'}>Video</MenuItem>
-                  </Select>
-                </FormControl>
-                <TextField
-                  label='Description'
-                  variant='outlined'
-                  multiline
-                  value={state?.node?.tags?.find((el: ITag) => el.name === 'Description')?.value}
-                  inputProps={{ readOnly: true }}
-                  style={{ width: '100%' }}
-                  margin='normal'
-                  sx={{ marginBottom: 0 }}
-                  minRows={2}
-                  maxRows={3}
-                />
-              </Box>
+  return (
+    <>
+      <Dialog open={true} maxWidth={'md'} fullWidth sx={{
+        '& .MuiPaper-root': {
+          background: 'rgba(61, 61, 61, 0.9)',
+          borderRadius: '30px',
+        }
+      }}>
+        <DialogTitle display='flex' justifyContent={'flex-end'} alignItems='center' lineHeight={0}>
+          <IconButton onClick={handleClose}>
+            <img src='/public/close-icon.svg'/>
+          </IconButton>
+        </DialogTitle>
+        <DialogContent sx={{ display: 'flex', gap: '48px', justifyContent: 'space-evenly'}}>
+          <Box sx={{
+            background: 'linear-gradient(to bottom, #000000 10%, rgba(71, 71, 71, 0) 100%)',
+            borderRadius: '23px',
+            backgroundPosition: 'center',
+            width: 'fit-content'
+          }}>
+            <img src={imgUrl} width='275px' height={'275px'}/>
+          </Box>
+          <Box display={'flex'} flexDirection={'column'} gap={'16px'}>
+            <Box>
+              <Typography sx={{
+                fontStyle: 'normal',
+                fontWeight: 700,
+                fontSize: '23px',
+                lineHeight: '31px',
+                display: 'flex',
+                alignItems: 'center',
+                textAlign: 'center',
+                color: '#FAFAFA',
+              }}>Name</Typography>
+              <Typography sx={{
+                fontStyle: 'normal',
+                fontWeight: 400,
+                fontSize: '23px',
+                lineHeight: '31px',
+                display: 'flex',
+                alignItems: 'center',
+                textAlign: 'center',
+                color: '#FAFAFA',
+              }}>{state?.node?.tags?.find((el: ITag) => el.name === 'Model-Name')?.value}</Typography>
             </Box>
-            <Divider textAlign='left'>
-              <Typography variant='h6'>Operators</Typography>
-            </Divider>
+            <Box>
+              <Typography sx={{
+                fontStyle: 'normal',
+                fontWeight: 700,
+                fontSize: '23px',
+                lineHeight: '31px',
+                display: 'flex',
+                alignItems: 'center',
+                textAlign: 'center',
+                color: '#FAFAFA',
+              }}>Category</Typography>
+              <Typography sx={{
+                fontStyle: 'normal',
+                fontWeight: 400,
+                fontSize: '23px',
+                lineHeight: '31px',
+                display: 'flex',
+                alignItems: 'center',
+                textAlign: 'center',
+                color: '#FAFAFA',
+              }}>{state?.node?.tags?.find((el: ITag) => el.name === 'Category')?.value}</Typography>
+            </Box>
+            <Box>
+              <Typography sx={{
+                fontStyle: 'normal',
+                fontWeight: 700,
+                fontSize: '23px',
+                lineHeight: '31px',
+                display: 'flex',
+                alignItems: 'center',
+                textAlign: 'center',
+                color: '#FAFAFA',
+              }}>Cost</Typography>
+              <Box display={'flex'} alignItems={'center'} justifyContent='space-between' width={'80%'} height='60px'>
+                <Typography sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  fontStyle: 'normal',
+                  fontWeight: 700,
+                  fontSize: '60px',
+                  lineHeight: '106px',
+                  textAlign: 'center',
+                  color: '#FAFAFA',
+                }}>{feeValue}</Typography>
+                <Icon sx={{ height: '50px', width: '50px'}}><img src='/public/arweave-logo.svg' width={'50px'} height={'50px'}/></Icon>
+              </Box>
+              
+            </Box>
+          </Box>
+          <Box display={'flex'} flexDirection={'column'} gap={'16px'}>
+            <Box>
+              <Typography sx={{
+                fontStyle: 'normal',
+                fontWeight: 700,
+                fontSize: '23px',
+                lineHeight: '31px',
+                display: 'flex',
+                alignItems: 'center',
+                textAlign: 'center',
+                color: '#FAFAFA',
+              }}>Description</Typography>
+              <Typography>{state?.node?.tags?.find((el: ITag) => el.name === 'Description')?.value || 'No Description Available.'}</Typography>
+            </Box>
+            <Button sx={{
+              border: '1px solid #FFFFFF',
+              borderRadius: '10px',
+              boxSizing: 'border-box'
+            }}><Typography>Stamp</Typography></Button>
+          </Box>
+        </DialogContent>
+        <DialogActions sx={{
+          justifyContent: 'center'
+        }}>
+          <Button sx={{
+            fontStyle: 'normal',
+            fontWeight: 700,
+            fontSize: '23px',
+            lineHeight: '31px',
+            display: 'flex',
+            alignItems: 'center',
+            textAlign: 'center',
+            color: '#FFFFFF',
+            borderRadius: '30px'
+          }} fullWidth onClick={() => setShowOperators(true)}>Choose an Operator</Button>
+        </DialogActions>
+        {
+          showOperators && <DialogContent>
             <BasicTable
               operators={operatorsData}
               loading={loading}
@@ -246,10 +244,10 @@ const Detail = () => {
               hasNextPage={hasNextPage}
               fetchMore={fetchMore}
             ></BasicTable>
-          </CardContent>
-        </Card>
-      </Box>
-    </Container>
+          </DialogContent>
+        }
+      </Dialog>
+    </>
   );
 };
 
