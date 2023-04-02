32076756863c48521966e0a32623fafcf74f0c49
l-silvestre <lcssilvestre98@gmail.com>
2023-04-02 17:27:00 +0100

    feat: implement operators screen design

--- client/src/pages/operators.tsx
+++ client/src/pages/operators.tsx
@@ -12,6 +12,8 @@ import {
   Typography,
   Button,
   Skeleton,
+  Select,
+  MenuItem,
 } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 import ReplayIcon from '@mui/icons-material/Replay';
@@ -24,6 +26,7 @@ const Operators = () => {
   const target = useRef<HTMLDivElement>(null);
   const isOnScreen = useOnScreen(target);
   const elementsPerPage = 5;
+  const [hightlightTop, setHighLightTop] = useState(false);
 
   const mockArray = genLoadingArray(6);
 
@@ -63,8 +66,119 @@ const Operators = () => {
     }
   }, [data]);
 
+  const handleHighlight = (value: boolean) => setHighLightTop(value);
+
   return (
-    <Container>
+    <Container sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      alignContent: 'space-around',
+      '@media all': {
+        maxWidth: '100%',
+      },
+    }}>
+      <Typography
+        sx={{
+          fontStyle: 'normal',
+          fontWeight: 300,
+          fontSize: '30px',
+          lineHeight: '41px',
+          /* identical to box height */
+          // background: 'linear-gradient(101.22deg, rgba(14, 255, 168, 0.58) 30.84%, #9747FF 55.47%, rgba(84, 81, 228, 0) 78.13%), linear-gradient(0deg, #FFFFFF, #FFFFFF)',
+        }}
+      >
+        Choose a Model to Start Operating
+      </Typography>
+      <Box className={'filter-box'} sx={{ display: 'flex' }}>
+        <Box display='flex' gap={'50px'}>
+          <Typography
+            sx={{
+              fontStyle: 'normal',
+              fongWeight: 500,
+              fontSize: '30px',
+              fontHeight: '41px',
+            }}
+            className={hightlightTop ? 'trending-text' : 'trending-text highlight'}
+            onClick={() => handleHighlight(false)}
+          >
+            Trending
+          </Typography>
+          <Typography
+            sx={{
+              fontStyle: 'normal',
+              fongWeight: 500,
+              fontSize: '30px',
+              fontHeight: '41px',
+            }}
+            className={hightlightTop ? 'trending-text highlight' : 'trending-text'}
+            onClick={() => handleHighlight(true)}
+          >
+            Top
+          </Typography>
+          <div className='underline'></div>
+          <Box flexGrow={1} />
+        </Box>
+        <Box flexGrow={1} />
+        <Box display='flex' gap={'50px'}>
+          <Select
+            sx={{
+              padding: '0px 8px',
+              border: '1px solid transparent',
+              borderRadius: '10px',
+              textTransform: 'none',
+              background:
+                'linear-gradient(#000, #000) padding-box, linear-gradient(170.66deg, rgba(14, 255, 168, 0.29) -38.15%, rgba(151, 71, 255, 0.5) 30.33%, rgba(84, 81, 228, 0) 93.33%) border-box',
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderWidth: 0,
+              },
+            }}
+            value={'24h'}
+          >
+            <MenuItem value={'24h'}>
+              <Typography
+                sx={{
+                  fontStyle: 'normal',
+                  fontWeight: 600,
+                  fontSize: '20px',
+                  lineHeight: '27px',
+                  textAlign: 'center',
+                  color: '#F4F4F4',
+                }}
+              >
+                24H
+              </Typography>
+            </MenuItem>
+            <MenuItem value={'week'}>
+              <Typography>1 Week</Typography>
+            </MenuItem>
+          </Select>
+          <Button
+            sx={{
+              borderRadius: '10px',
+              border: '1px solid transparent',
+              padding: '8px',
+              textTransform: 'none',
+              background:
+                'linear-gradient(#000, #000) padding-box, linear-gradient(170.66deg, rgba(14, 255, 168, 0.29) -38.15%, rgba(151, 71, 255, 0.5) 30.33%, rgba(84, 81, 228, 0) 93.33%) border-box',
+            }}
+          >
+            <Typography
+              sx={{
+                padding: '0px 8px',
+                fontStyle: 'normal',
+                fontWeight: 600,
+                fontSize: '20px',
+                lineHeight: '27px',
+                textAlign: 'center',
+                color: '#F4F4F4',
+              }}
+            >
+              View All
+            </Typography>
+          </Button>
+        </Box>
+      </Box>
       <Box>
         <Stack spacing={4} sx={{ margin: '16px' }}>
           {error ? (
@@ -81,7 +195,7 @@ const Operators = () => {
               </Typography>
             </Container>
           ) : (
-            txs.map((el: IEdge) => <ModelCard modelTx={el} key={el.node.id} />)
+            txs.map((el: IEdge, idx: number) => <ModelCard modelTx={el} key={el.node.id} index={idx}/>)
           )}
           {loading &&
             mockArray.map((val) => (
