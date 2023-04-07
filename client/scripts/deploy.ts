eeb39060d945f81e6c05315974f6b9620cfd0b6f
l-silvestre <lcssilvestre98@gmail.com>
2023-04-07 13:39:42 +0100

    fix: fix deploy script and add deploy command to package.json

--- client/scripts/deploy.ts
+++ client/scripts/deploy.ts
@@ -4,14 +4,15 @@ import Bundlr from 'bundlr-custom';
 import fs from 'fs';
 
 const main = async () => {
-  const privateKey = '../wallet.json';
+  const wallet = './wallet.json';
 
-  const jwk = JSON.parse(fs.readFileSync(privateKey).toString());
+  const jwk = JSON.parse(fs.readFileSync(wallet).toString());
   
   // NOTE: Depending on the version of JavaScript you use, you may need to use
   // the commented out line below to create a new Bundlr object.
   // const bundlr = new Bundlr("http://node1.bundlr.network", "arweave", jwk);
-  const bundlr = new Bundlr('http://node1.bundlr.network', 'arweave', jwk);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const bundlr = new (Bundlr as any).default('http://node1.bundlr.network', 'arweave', jwk );
   
   // Get loaded balance in atomic units
   const atomicBalance = await bundlr.getLoadedBalance();
@@ -23,9 +24,9 @@ const main = async () => {
   
   // Print your wallet address
   console.log(`wallet address = ${bundlr.address}`);
-  const dist = './../dist/';
+  const dist = './dist/';
   const response = await bundlr.uploadFolder(dist, {
-    indexFile: './../dist/index.html', // optional index file (file the user will load when accessing the manifest)
+    indexFile: './dist/index.html', // optional index file (file the user will load when accessing the manifest)
     batchSize: 50, // number of items to upload at once
     keepDeleted: false   // whether to keep now deleted items from previous uploads
   }); // returns the manifest ID
