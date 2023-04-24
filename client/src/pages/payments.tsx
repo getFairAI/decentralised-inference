bd143abbb97d628d50ce28258dc8b9a94f1e380c
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-04-24 16:50:18 +0100

    feat: implement filters for payments screen, disable dates for now

--- client/src/pages/payments.tsx
+++ client/src/pages/payments.tsx
@@ -6,20 +6,26 @@ import { IEdge } from '@/interfaces/arweave';
 import { QUERY_USER_INTERACTIONS } from '@/queries/graphql';
 import arweave from '@/utils/arweave';
 import { useQuery } from '@apollo/client';
-import { Box, Typography, Button, Backdrop, CircularProgress, useTheme, Container, Stack } from '@mui/material';
-import { useContext, useState, useEffect, useRef } from 'react';
+import { Box, Typography, Button, Backdrop, CircularProgress, useTheme, Container, Stack, InputBase, Icon } from '@mui/material';
+import { useContext, useState, useEffect, useRef, ChangeEvent } from 'react';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import _ from 'lodash';
+import { findTag } from '@/utils/common';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const Payments = () => {
   const elementsPerPage = 10;
   const { currentAddress } = useContext(WalletContext);
   const [minHeight, setMinHeight] = useState(0);
   const [hasNextPage, setHasNextPage ] = useState(false);
+  const [ filterValue, setFilterValue ] = useState('');
+  const [ filteredValues, setFilteredValues ] = useState<IEdge[]>([]);
+  const [ startDateFilter, setStartDateFilter ] = useState<string | null>(null);
+  const [ endDateFilter, setEndDateFilter ] = useState<string | null>(null);
   const theme = useTheme();
   const target = useRef<HTMLDivElement>(null);
   const isOnScreen = useOnScreen(target);
-
 
   const { data, previousData, error, loading, refetch, fetchMore } = useQuery(QUERY_USER_INTERACTIONS, {
     variables: {
@@ -88,12 +94,33 @@ const Payments = () => {
     }
   }, [ isOnScreen, hasNextPage ]);
 
+  useEffect(() => {
+    if (filterValue && data) {
+      const filteredData = data.transactions.edges.filter((el: IEdge) => 
+      el.node.id.toLowerCase().indexOf(filterValue) !== -1 ||
+        el.node.recipient.toLowerCase().indexOf(filterValue) !== -1 ||
+          findTag(el, 'operationName')?.toLowerCase().indexOf(filterValue) !== -1
+      );
+      setFilteredValues(filteredData);
+    } else if (data) {
+      setFilteredValues(data.transactions.edges);
+    } else {
+      setFilteredValues([]);
+    }
+  }, [ filterValue, data ]);
+
+  useEffect(() => {
+    /* console.log(startDateFilter);
+    console.log(endDateFilter); */
+  }, [ startDateFilter, endDateFilter ]);
+
   const refreshClick = () => {
     refetch();
   };
 
   return (
-    <Container>
+    
+    <Container sx={{ paddingTop: '16px'}} maxWidth='lg'>
       {error ? (
         <Box display={'flex'} flexDirection={'column'} alignItems={'center'} padding={'16px'}>
           <Typography textAlign={'center'}>
@@ -106,13 +133,63 @@ const Payments = () => {
         </Box>
       ) : (
         <>
-          <Box display={'flex'} justifyContent={'center'} padding={'8px'}>
+          <Box paddingLeft={'8px'}>
             <Button onClick={refreshClick} endIcon={<RefreshIcon />} variant='outlined'>
               <Typography>Refresh</Typography>
             </Button>
           </Box>
+          <Box display={'flex'} justifyContent={'space-between'} padding={'16px 8px'}>
+            <Box display={'flex'} gap={'8px'}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Start Date"
+                  value={startDateFilter}
+                  disabled
+                  onChange={(newValue: string | null) => setStartDateFilter(newValue)}
+                />
+                <DatePicker
+                  label="End Date"
+                  value={endDateFilter}
+                  disabled
+                  onChange={(newValue: string | null) => setEndDateFilter(newValue)}
+                />
+              </LocalizationProvider>
+            </Box>
+            <Box
+              width={'40%'}
+              sx={{
+                borderRadius: '30px',
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: '3px 20px 3px 0px',
+                alignItems: 'center',
+                background: theme.palette.background.default,
+              }}
+            >
+              <InputBase
+                sx={{
+                  fontStyle: 'normal',
+                  fontWeight: 400,
+                  fontSize: '18px',
+                  lineHeight: '16px',
+                  width: '100%',
+                }}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  setFilterValue(event.target.value)
+                }
+                placeholder='Search By Id, Recipient or Operation Name'
+              />
+              <Icon
+                sx={{
+                  height: '30px',
+                }}
+              >
+                <img src='./search-icon.svg'></img>
+              </Icon>
+            </Box>
+          </Box>
           <Stack spacing={2}>
-              {data && data.transactions.edges.map((tx: IEdge) => <PendingCard tx={tx} key={tx.node.id} autoRetry={false}/>)}
+              {filteredValues.map((tx: IEdge) => <PendingCard tx={tx} key={tx.node.id} autoRetry={false}/>)}
           </Stack>
         </>
       )}
