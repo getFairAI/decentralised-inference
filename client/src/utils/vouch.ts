89e4fe5d57da1c5883f0be4997094028ed27e206
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-05-04 13:20:33 +0100

    chore: replace vouchdao package with vouch source code

---
+++ client/src/utils/vouch.ts
@@ -0,0 +1,24 @@
+import { WarpFactory } from 'warp-contracts';
+
+const warp = WarpFactory.forMainnet();
+
+const contract = warp
+  .contract('_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk')
+  .connect('use_wallet')
+  .setEvaluationOptions({
+    allowBigInt: true,
+  });
+
+interface VouchState {
+  state: {
+    vouched: {
+      [address: string]: unknown,
+    }
+  }
+};
+
+export const isVouched = async (address: string) => {
+  const { cachedValue } = await contract.readState();
+
+  return (cachedValue as VouchState).state.vouched[address] !== undefined;
+};
