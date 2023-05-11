bcb5ddce66bb516d35a3f53ce355d5bf1eeedf68
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-05-11 16:50:19 +0100

    fix: files formatting

--- client/src/components/chat-bubble.tsx
+++ client/src/components/chat-bubble.tsx
@@ -58,95 +58,103 @@ const ChatBubble = ({
   const theme = useTheme();
 
   const waitingResponseFragment = () => {
-    return <>
-      {
-        isWaitingResponse && !responseTimeout && <Container maxWidth={false} sx={{ paddingTop: '16px' }}>
-          <Stack spacing={4} flexDirection='row'>
-            <Box display={'flex'} flexDirection='column' margin='8px' width='100%'>
-              <Box display={'flex'} alignItems='center' justifyContent={defaultJustifyContent}>
-                <Card
-                  elevation={8}
-                  raised={true}
-                  sx={{
-                    width: 'fit-content',
-                    maxWidth: '75%',
-                    // background: el.type === 'response' ? 'rgba(96, 96, 96, 0.7);' : 'rgba(52, 52, 52, 0.7);',
-                    border: '4px solid transparent',
-                    background:
-                      theme.palette.mode === 'dark'
-                        ? 'rgba(52, 52, 52, 0.7)'
-                        : theme.palette.secondary.main,
-                    // opacity: '0.4',
-                    borderRadius: '40px',
-                  }}
-                >
-                  <CardContent
+    return (
+      <>
+        {isWaitingResponse && !responseTimeout && (
+          <Container maxWidth={false} sx={{ paddingTop: '16px' }}>
+            <Stack spacing={4} flexDirection='row'>
+              <Box display={'flex'} flexDirection='column' margin='8px' width='100%'>
+                <Box display={'flex'} alignItems='center' justifyContent={defaultJustifyContent}>
+                  <Card
+                    elevation={8}
+                    raised={true}
                     sx={{
-                      padding: '24px 32px',
-                      gap: '16px',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: defaultJustifyContent,
+                      width: 'fit-content',
+                      maxWidth: '75%',
+                      // background: el.type === 'response' ? 'rgba(96, 96, 96, 0.7);' : 'rgba(52, 52, 52, 0.7);',
+                      border: '4px solid transparent',
+                      background:
+                        theme.palette.mode === 'dark'
+                          ? 'rgba(52, 52, 52, 0.7)'
+                          : theme.palette.secondary.main,
+                      // opacity: '0.4',
+                      borderRadius: '40px',
                     }}
                   >
-                    <LoadingContainer className='dot-pulse' sx={{ marginBottom: '0.35em' }} />
-                  </CardContent>
-                </Card>
+                    <CardContent
+                      sx={{
+                        padding: '24px 32px',
+                        gap: '16px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: defaultJustifyContent,
+                      }}
+                    >
+                      <LoadingContainer className='dot-pulse' sx={{ marginBottom: '0.35em' }} />
+                    </CardContent>
+                  </Card>
+                </Box>
               </Box>
-            </Box>
-          </Stack>
-        </Container>
-      }
-    </>;
+            </Stack>
+          </Container>
+        )}
+      </>
+    );
   };
 
   const showDayDivider = (el: IMessage, index: number) => {
-    const daysDiffer = index < messages.length - 1 &&
-      new Date(el.timestamp * secondInMS).getDay() !== new Date(messages[index + 1].timestamp * secondInMS).getDay();
-    return <>
-      {
-        daysDiffer && <Divider textAlign='center'>
-          <Typography
-            sx={{
-              fontStyle: 'normal',
-              fontWeight: 300,
-              fontSize: '20px',
-              lineHeight: '27px',
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            {new Date(messages[index + 1].timestamp * secondInMS).toLocaleDateString()}
-          </Typography>
-        </Divider>
-      }
-    </>;
+    const daysDiffer =
+      index < messages.length - 1 &&
+      new Date(el.timestamp * secondInMS).getDay() !==
+        new Date(messages[index + 1].timestamp * secondInMS).getDay();
+    return (
+      <>
+        {daysDiffer && (
+          <Divider textAlign='center'>
+            <Typography
+              sx={{
+                fontStyle: 'normal',
+                fontWeight: 300,
+                fontSize: '20px',
+                lineHeight: '27px',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              {new Date(messages[index + 1].timestamp * secondInMS).toLocaleDateString()}
+            </Typography>
+          </Divider>
+        )}
+      </>
+    );
   };
 
   const showResponseTimeoutFragment = () => {
     if (responseTimeout && !isWaitingResponse) {
-      return <Container maxWidth={false} sx={{ paddingTop: '16px' }}>
-        <Stack spacing={4} flexDirection='row'>
-          <Box display={'flex'} flexDirection='column' margin='8px' width='100%'>
-            <Box display={'flex'} alignItems='center' justifyContent={'center'}>
-              <Typography
-                sx={{
-                  fontStyle: 'normal',
-                  fontWeight: 600,
-                  fontSize: '30px',
-                  lineHeight: '41px',
-                  display: 'block',
-                  textAlign: 'center',
-                  color: '#F4BA61',
-                }}
-              >
-                The last request has not received a response in the defined amount of time,
-                please consider retrying with a new operator
-              </Typography>
+      return (
+        <Container maxWidth={false} sx={{ paddingTop: '16px' }}>
+          <Stack spacing={4} flexDirection='row'>
+            <Box display={'flex'} flexDirection='column' margin='8px' width='100%'>
+              <Box display={'flex'} alignItems='center' justifyContent={'center'}>
+                <Typography
+                  sx={{
+                    fontStyle: 'normal',
+                    fontWeight: 600,
+                    fontSize: '30px',
+                    lineHeight: '41px',
+                    display: 'block',
+                    textAlign: 'center',
+                    color: '#F4BA61',
+                  }}
+                >
+                  The last request has not received a response in the defined amount of time, please
+                  consider retrying with a new operator
+                </Typography>
+              </Box>
             </Box>
-          </Box>
-        </Stack>
-      </Container>;
+          </Stack>
+        </Container>
+      );
     } else {
       return <></>;
     }
@@ -154,26 +162,28 @@ const ChatBubble = ({
 
   const hasNoErrorsFragment = () => {
     if (messages.length > 0 && !messagesLoading) {
-      return <>
-        <Divider textAlign='center' sx={{ ml: '24px', mr: '24px' }}>
-          {new Date(messages[0].timestamp * secondInMS).toLocaleDateString()}
-        </Divider>
-        {messages.map((el: IMessage, index: number) => (
-          <Container
-            key={el.id}
-            maxWidth={false}
-            sx={{ paddingTop: '16px' }}
-            className='message-container'
-          >
-            <Message message={el} index={index} pendingTxs={pendingTxs} />
-            {showDayDivider(el, index)}
-          </Container>
-        ))}
-        {waitingResponseFragment()}
-        {showResponseTimeoutFragment()}
-      </>;
+      return (
+        <>
+          <Divider textAlign='center' sx={{ ml: '24px', mr: '24px' }}>
+            {new Date(messages[0].timestamp * secondInMS).toLocaleDateString()}
+          </Divider>
+          {messages.map((el: IMessage, index: number) => (
+            <Container
+              key={el.id}
+              maxWidth={false}
+              sx={{ paddingTop: '16px' }}
+              className='message-container'
+            >
+              <Message message={el} index={index} pendingTxs={pendingTxs} />
+              {showDayDivider(el, index)}
+            </Container>
+          ))}
+          {waitingResponseFragment()}
+          {showResponseTimeoutFragment()}
+        </>
+      );
     }
-    
+
     return hasNoMessagesFragment();
   };
 
