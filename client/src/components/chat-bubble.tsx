ba0f5d3fdac7a212c6d7615c79dca66c3458928f
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-05-11 16:50:03 +0100

    fix: more sonar lint issues on new component

--- client/src/components/chat-bubble.tsx
+++ client/src/components/chat-bubble.tsx
@@ -32,6 +32,7 @@ import {
 } from '@mui/material';
 import Message from './message';
 import Transaction from 'arweave/node/lib/transaction';
+import { secondInMS } from '@/constants';
 
 const ChatBubble = ({
   showLoading,
@@ -50,14 +51,112 @@ const ChatBubble = ({
   pendingTxs: Transaction[];
   messagesLoading: boolean;
 }) => {
-  const mockArray = genLoadingArray(5);
+  const mockElements = 5;
+  const pairDivider = 2; // number to divide by to check if even or odd
+  const defaultJustifyContent = 'flex-start';
+  const mockArray = genLoadingArray(mockElements);
   const theme = useTheme();
 
+  const waitingResponseFragment = () => {
+    return <>
+      {
+        isWaitingResponse && !responseTimeout && <Container maxWidth={false} sx={{ paddingTop: '16px' }}>
+          <Stack spacing={4} flexDirection='row'>
+            <Box display={'flex'} flexDirection='column' margin='8px' width='100%'>
+              <Box display={'flex'} alignItems='center' justifyContent={defaultJustifyContent}>
+                <Card
+                  elevation={8}
+                  raised={true}
+                  sx={{
+                    width: 'fit-content',
+                    maxWidth: '75%',
+                    // background: el.type === 'response' ? 'rgba(96, 96, 96, 0.7);' : 'rgba(52, 52, 52, 0.7);',
+                    border: '4px solid transparent',
+                    background:
+                      theme.palette.mode === 'dark'
+                        ? 'rgba(52, 52, 52, 0.7)'
+                        : theme.palette.secondary.main,
+                    // opacity: '0.4',
+                    borderRadius: '40px',
+                  }}
+                >
+                  <CardContent
+                    sx={{
+                      padding: '24px 32px',
+                      gap: '16px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: defaultJustifyContent,
+                    }}
+                  >
+                    <LoadingContainer className='dot-pulse' sx={{ marginBottom: '0.35em' }} />
+                  </CardContent>
+                </Card>
+              </Box>
+            </Box>
+          </Stack>
+        </Container>
+      }
+    </>;
+  };
+
+  const showDayDivider = (el: IMessage, index: number) => {
+    const daysDiffer = index < messages.length - 1 &&
+      new Date(el.timestamp * secondInMS).getDay() !== new Date(messages[index + 1].timestamp * secondInMS).getDay();
+    return <>
+      {
+        daysDiffer && <Divider textAlign='center'>
+          <Typography
+            sx={{
+              fontStyle: 'normal',
+              fontWeight: 300,
+              fontSize: '20px',
+              lineHeight: '27px',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            {new Date(messages[index + 1].timestamp * secondInMS).toLocaleDateString()}
+          </Typography>
+        </Divider>
+      }
+    </>;
+  };
+
+  const showResponseTimeoutFragment = () => {
+    if (responseTimeout && !isWaitingResponse) {
+      return <Container maxWidth={false} sx={{ paddingTop: '16px' }}>
+        <Stack spacing={4} flexDirection='row'>
+          <Box display={'flex'} flexDirection='column' margin='8px' width='100%'>
+            <Box display={'flex'} alignItems='center' justifyContent={'center'}>
+              <Typography
+                sx={{
+                  fontStyle: 'normal',
+                  fontWeight: 600,
+                  fontSize: '30px',
+                  lineHeight: '41px',
+                  display: 'block',
+                  textAlign: 'center',
+                  color: '#F4BA61',
+                }}
+              >
+                The last request has not received a response in the defined amount of time,
+                please consider retrying with a new operator
+              </Typography>
+            </Box>
+          </Box>
+        </Stack>
+      </Container>;
+    } else {
+      return <></>;
+    }
+  };
+
   const hasNoErrorsFragment = () => {
-    return messages.length > 0 && !messagesLoading ? (
-      <>
+    if (messages.length > 0 && !messagesLoading) {
+      return <>
         <Divider textAlign='center' sx={{ ml: '24px', mr: '24px' }}>
-          {new Date(messages[0].timestamp * 1000).toLocaleDateString()}
+          {new Date(messages[0].timestamp * secondInMS).toLocaleDateString()}
         </Divider>
         {messages.map((el: IMessage, index: number) => (
           <Container
@@ -67,92 +166,15 @@ const ChatBubble = ({
             className='message-container'
           >
             <Message message={el} index={index} pendingTxs={pendingTxs} />
-            {index < messages.length - 1 &&
-              new Date(el.timestamp * 1000).getDay() !==
-                new Date(messages[index + 1].timestamp * 1000).getDay() && (
-                <Divider textAlign='center'>
-                  <Typography
-                    sx={{
-                      fontStyle: 'normal',
-                      fontWeight: 300,
-                      fontSize: '20px',
-                      lineHeight: '27px',
-                      display: 'flex',
-                      alignItems: 'center',
-                    }}
-                  >
-                    {new Date(messages[index + 1].timestamp * 1000).toLocaleDateString()}
-                  </Typography>
-                </Divider>
-              )}
+            {showDayDivider(el, index)}
           </Container>
         ))}
-        {isWaitingResponse && !responseTimeout && (
-          <Container maxWidth={false} sx={{ paddingTop: '16px' }}>
-            <Stack spacing={4} flexDirection='row'>
-              <Box display={'flex'} flexDirection='column' margin='8px' width='100%'>
-                <Box display={'flex'} alignItems='center' justifyContent={'flex-start'}>
-                  <Card
-                    elevation={8}
-                    raised={true}
-                    sx={{
-                      width: 'fit-content',
-                      maxWidth: '75%',
-                      // background: el.type === 'response' ? 'rgba(96, 96, 96, 0.7);' : 'rgba(52, 52, 52, 0.7);',
-                      border: '4px solid transparent',
-                      background:
-                        theme.palette.mode === 'dark'
-                          ? 'rgba(52, 52, 52, 0.7)'
-                          : theme.palette.secondary.main,
-                      // opacity: '0.4',
-                      borderRadius: '40px',
-                    }}
-                  >
-                    <CardContent
-                      sx={{
-                        padding: '24px 32px',
-                        gap: '16px',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'flex-start',
-                      }}
-                    >
-                      <LoadingContainer className='dot-pulse' sx={{ marginBottom: '0.35em' }} />
-                    </CardContent>
-                  </Card>
-                </Box>
-              </Box>
-            </Stack>
-          </Container>
-        )}
-        {responseTimeout && !isWaitingResponse && (
-          <Container maxWidth={false} sx={{ paddingTop: '16px' }}>
-            <Stack spacing={4} flexDirection='row'>
-              <Box display={'flex'} flexDirection='column' margin='8px' width='100%'>
-                <Box display={'flex'} alignItems='center' justifyContent={'center'}>
-                  <Typography
-                    sx={{
-                      fontStyle: 'normal',
-                      fontWeight: 600,
-                      fontSize: '30px',
-                      lineHeight: '41px',
-                      display: 'block',
-                      textAlign: 'center',
-                      color: '#F4BA61',
-                    }}
-                  >
-                    The last request has not received a response in the defined amount of time,
-                    please consider retrying with a new operator
-                  </Typography>
-                </Box>
-              </Box>
-            </Stack>
-          </Container>
-        )}
-      </>
-    ) : (
-      hasNoMessagesFragment()
-    );
+        {waitingResponseFragment()}
+        {showResponseTimeoutFragment()}
+      </>;
+    }
+    
+    return hasNoMessagesFragment();
   };
 
   const hasNoMessagesFragment = () => {
@@ -174,12 +196,12 @@ const ChatBubble = ({
               <Stack
                 spacing={4}
                 flexDirection='row'
-                justifyContent={el % 2 === 0 ? 'flex-end' : 'flex-start'}
+                justifyContent={el % pairDivider === 0 ? 'flex-end' : defaultJustifyContent}
               >
                 <Skeleton animation={'wave'} width={'40%'}>
                   <Box
                     display={'flex'}
-                    justifyContent={el % 2 === 0 ? 'flex-end' : 'flex-start'}
+                    justifyContent={el % pairDivider === 0 ? 'flex-end' : defaultJustifyContent}
                     flexDirection='column'
                     margin='8px'
                   >
