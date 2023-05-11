d6aa182bd6ae296861caf3de9ff15e4e609ab1f5
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-05-11 23:04:25 +0100

    fix: update navigation menu links and solve sonar code smells

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,3 +1,21 @@
+/*
+ * Fair Protocol, open source decentralised inference marketplace for artificial intelligence.
+ * Copyright (C) 2023 Fair Protocol
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -136,7 +154,8 @@ const Navbar = ({
   const { pathname, state } = useLocation();
   const navigate = useNavigate();
   const theme = useTheme();
-  const zIndex = theme.zIndex.drawer + 2; // add 2 to make sure it's above the drawer
+  const extraIndex = 2; // number to add to zIndex to make sure it's above the drawer
+  const zIndex = theme.zIndex.drawer + extraIndex; // add 2 to make sure it's above the drawer
   const navbarLinkStyles = {
     fontWeight: 400,
     fontSize: '18px',
--- client/src/components/navigation-menu.tsx
+++ client/src/components/navigation-menu.tsx
@@ -1,5 +1,23 @@
+/*
+ * Fair Protocol, open source decentralised inference marketplace for artificial intelligence.
+ * Copyright (C) 2023 Fair Protocol
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
 import { Box, Button, Menu, MenuItem, SxProps, Typography, useTheme } from '@mui/material';
-import { useState, MouseEvent } from 'react';
+import { useState, MouseEvent, useCallback } from 'react';
 import { NavLink } from 'react-router-dom';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
@@ -7,13 +25,16 @@ const NavigationMenu = ({ navStyles }: { navStyles: SxProps }) => {
   const theme = useTheme();
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
 
-  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+  const handleOpenNavMenu = useCallback(
+    (event: MouseEvent<HTMLElement>) => {
+      setAnchorElNav(event.currentTarget);
+    },
+    [setAnchorElNav],
+  );
 
-  const handleCloseNavMenu = () => {
+  const handleCloseNavMenu = useCallback(() => {
     setAnchorElNav(null);
-  };
+  }, [setAnchorElNav]);
 
   return (
     <Box sx={{ display: { sm: 'flex', md: 'none' } }}>
@@ -54,7 +75,9 @@ const NavigationMenu = ({ navStyles }: { navStyles: SxProps }) => {
         }}
       >
         <MenuItem key={'explore'} onClick={handleCloseNavMenu}>
-          <Typography textAlign={'center'}>Explore</Typography>
+          <Typography component={NavLink} to='/'>
+            Explore
+          </Typography>
         </MenuItem>
         <MenuItem key={'creators'} onClick={handleCloseNavMenu}>
           <Typography component={NavLink} to='/upload-creator'>
@@ -62,12 +85,12 @@ const NavigationMenu = ({ navStyles }: { navStyles: SxProps }) => {
           </Typography>
         </MenuItem>
         <MenuItem key={'curators'} onClick={handleCloseNavMenu}>
-          <Typography component={NavLink} to='/upload-creator'>
+          <Typography component={NavLink} to='/upload'>
             Curators
           </Typography>
         </MenuItem>
         <MenuItem key={'operators'} onClick={handleCloseNavMenu}>
-          <Typography component={NavLink} to='/upload-creator'>
+          <Typography component={NavLink} to='/operators'>
             Operators
           </Typography>
         </MenuItem>
