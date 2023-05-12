3c342e9d7bfa069a5e3fcbffcf98169d04a5d5fc
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-05-12 14:40:56 +0100

    fix: change base path for ci build [skip ci]

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,10 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import eslint from 'vite-plugin-eslint';
 
 const ci = process.env.CI ?? false;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: ci ? './decentralized-inference/' : './',
+  base: ci ? '/decentralized-inference/' : './',
   plugins: [react(), tsconfigPaths(), nodePolyfills({ protocolImports: true }), eslint()],
   optimizeDeps: {disabled: false},
   build: {
