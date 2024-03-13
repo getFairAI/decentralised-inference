947d4236954e840958ec8e59aa45e12339e166ae
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2024-03-13 14:10:29 +0000

    feat: update logo

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -21,7 +21,7 @@ import { Icon } from '@mui/material';
 const Logo = () => {
   return (
     <Icon sx={{ height: '100%', width: '100%', display: 'flex' }}>
-      <img src={'./fair-protocol-outline.svg'} style={{ color: '#1F1F26' }} alt='' />
+      <img src={'./fair-ai-outline.svg'} alt='' />
     </Icon>
   );
 };
