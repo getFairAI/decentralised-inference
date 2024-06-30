49ff7313630dc0b709f566b29ec64957d884b120
azenyr <61697668+azenyr@users.noreply.github.com>
2024-06-30 12:45:34 +0100

    feat: better wallet selection UI

--- client/src/components/choose-wallet.tsx
+++ client/src/components/choose-wallet.tsx
@@ -26,10 +26,11 @@ import {
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Tooltip,
   Typography,
   useTheme,
 } from '@mui/material';
-import { Dispatch, SetStateAction, useCallback, useContext } from 'react';
+import { Dispatch, SetStateAction, useCallback, useContext, useState } from 'react';
 import { EVMWalletContext } from '@/context/evm-wallet';
 import { EIP6963ProviderDetail } from '@/interfaces/evm';
 import { EIP1193Provider } from 'viem';
@@ -40,7 +41,12 @@ import { StyledMuiButton } from '@/styles/components';
 import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
 import WalletRoundedIcon from '@mui/icons-material/WalletRounded';
 import ExitToAppRoundedIcon from '@mui/icons-material/ExitToAppRounded';
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
 import { motion } from 'framer-motion';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { enqueueSnackbar } from 'notistack';
 
 const ProviderElement = ({
   provider,
@@ -61,30 +67,185 @@ const ProviderElement = ({
     setOpen(false);
   }, [connect]);
 
+  const [moreInfoWalletOpened, openMoreInfoWallet] = useState(false);
+
   return (
-    <div className='rounded-3xl bg-slate-300 py-2'>
+    <div className='rounded-3xl bg-slate-300 py-2 my-2'>
+      {provider.info.name !== 'Rabby Wallet' && (
+        <ListItem
+          key={provider.info.uuid}
+          secondaryAction={
+            <StyledMuiButton
+              aria-label='Connect this wallet'
+              onClick={handleEvmConnect}
+              disabled={currentProviderValue === provider.info.name}
+              className='plausible-event-name=EVM+Connected primary'
+            >
+              {currentProviderValue === provider.info.name ? 'Connected' : 'Connect'}
+              <ExitToAppRoundedIcon style={{ width: '22px' }} />
+            </StyledMuiButton>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar
+              src={provider.info.icon}
+              alt='provider.info.name'
+              className='p-[6px] object-contain bg-white'
+            />
+          </ListItemAvatar>
+          <ListItemText primary={provider.info.name} />
+        </ListItem>
+      )}
+
+      {provider.info.name === 'Rabby Wallet' && (
+        <>
+          <ListItem
+            key={provider.info.uuid}
+            secondaryAction={
+              <StyledMuiButton
+                aria-label='Connect this wallet'
+                onClick={() => {
+                  openMoreInfoWallet(!moreInfoWalletOpened);
+                }}
+                className='plausible-event-name=EVM+Connected secondary'
+              >
+                {currentProviderValue === provider.info.name ? 'Connected' : 'More Info'}
+                <KeyboardArrowDownRoundedIcon style={{ width: '22px' }} />
+              </StyledMuiButton>
+            }
+          >
+            <ListItemAvatar>
+              <Avatar
+                src={provider.info.icon}
+                alt='provider.info.name'
+                className='p-[6px] object-contain bg-white'
+              />
+            </ListItemAvatar>
+            <ListItemText primary={provider.info.name} />
+          </ListItem>
+
+          {moreInfoWalletOpened && (
+            <motion.div
+              initial={{ opacity: 0, padding: 0, y: '-60px', height: 0 }}
+              animate={{
+                opacity: 1,
+                padding: '10px 30px',
+                height: 'max-content',
+                y: 0,
+                transition: {
+                  duration: 0.2,
+                },
+              }}
+              className='flex flex-col gap-4'
+            >
+              <div className='font-semibold'>This wallet needs a few extra configurations:</div>
+              <p>{'1. Click on the Rabby extension, and then click on "more".'}</p>
+              <div className='w-full p-2 flex justify-center'>
+                <img
+                  src='./wallet-instructions/rabby-1.jpg'
+                  className='w-full max-w-[400px] rounded-3xl shadow-lg'
+                />
+              </div>
+              <p>{'2. In the settings section, click on the "Modify RPC URL" option.'}</p>
+              <div className='w-full p-2 flex justify-center'>
+                <img
+                  src='./wallet-instructions/rabby-2.jpg'
+                  className='w-full max-w-[400px] rounded-3xl shadow-lg'
+                />
+              </div>
+              <p>
+                {
+                  '3. Click on the "Modify RPC URL" option and then click on the "Arbitrum" network.'
+                }
+              </p>
+              <div className='w-full p-2 flex justify-center'>
+                <img
+                  src='./wallet-instructions/rabby-3.jpg'
+                  className='w-full max-w-[400px] rounded-3xl shadow-lg'
+                />
+              </div>
+              <p>
+                {'4. Add the URL '}
+                <CopyToClipboard
+                  text={'https://arb1.arbitrum.io/rpc'}
+                  onCopy={() => {
+                    enqueueSnackbar('Copied to clipboard.', {
+                      variant: 'success',
+                      autoHideDuration: 2000,
+                      style: { fontWeight: 700 },
+                    });
+                  }}
+                >
+                  <Tooltip title={'Click to copy'}>
+                    <span className='cursor-pointer bg-slate-600 py-1 px-2 rounded-xl text-white hover:bg-slate-500'>
+                      https://arb1.arbitrum.io/rpc
+                      <ContentCopyRoundedIcon
+                        style={{ width: '20px', marginLeft: '5px', marginBottom: '3px' }}
+                      />
+                    </span>
+                  </Tooltip>
+                </CopyToClipboard>
+                {' and click Save.'}
+              </p>
+              <div className='w-full p-2 flex justify-center'>
+                <img
+                  src='./wallet-instructions/rabby-4.jpg'
+                  className='w-full max-w-[400px] rounded-3xl shadow-lg'
+                />
+              </div>
+              <p>
+                <strong>Note: </strong> It may be necessary to clear the cache (CTRL+SHIFT+R) to
+                apply the changes.
+              </p>
+
+              <div className='flex justify-end pb-3'>
+                <StyledMuiButton
+                  aria-label='Connect this wallet'
+                  onClick={handleEvmConnect}
+                  disabled={currentProviderValue === provider.info.name}
+                  className='plausible-event-name=EVM+Connected primary'
+                >
+                  {currentProviderValue === provider.info.name
+                    ? 'Connected'
+                    : 'Confirm and Connect'}
+                  <ExitToAppRoundedIcon style={{ width: '22px' }} />
+                </StyledMuiButton>
+              </div>
+            </motion.div>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+const InstallMetaMaskElement = () => {
+  const installWallet = () => {
+    window.open('https://metamask.io/download/', '_blank');
+  };
+
+  return (
+    <div className='rounded-3xl bg-slate-300 py-2 my-2'>
       <ListItem
-        key={provider.info.uuid}
         secondaryAction={
           <StyledMuiButton
-            aria-label='Connect this wallet'
-            onClick={handleEvmConnect}
-            disabled={currentProviderValue === provider.info.name}
-            className='plausible-event-name=EVM+Connected primary'
+            aria-label='Install this wallet'
+            onClick={installWallet}
+            className='plausible-event-name=EVM+Connected secondary'
           >
-            {currentProviderValue === provider.info.name ? 'Connected' : 'Connect'}
-            <ExitToAppRoundedIcon style={{ width: '22px' }} />
+            Install
+            <OpenInNewRoundedIcon style={{ width: '22px' }} />
           </StyledMuiButton>
         }
       >
         <ListItemAvatar>
           <Avatar
-            src={provider.info.icon}
+            src={'./icons/metamask.svg'}
             alt='provider.info.name'
             className='p-[6px] object-contain bg-white'
           />
         </ListItemAvatar>
-        <ListItemText primary={provider.info.name} className='font-semibold' />
+        <ListItemText primary={'MetaMask'} />
       </ListItem>
     </div>
   );
@@ -101,6 +262,8 @@ const ChooseWallet = ({
   const theme = useTheme();
   const { providers } = useContext(EVMWalletContext);
   const handleClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const metaMaskProviderFound = providers.find((provider) => provider.info.name === 'MetaMask');
 
   return (
     <Dialog
@@ -136,9 +299,31 @@ const ChooseWallet = ({
           animate={{ x: 0, opacity: 1, transition: { delay: 0.2, duration: 0.4 } }}
         >
           <List>
-            {providers.map((provider) => (
-              <ProviderElement provider={provider} key={provider.info.uuid} setOpen={setOpen} />
-            ))}
+            <div className='font-semibold font-xl'>Our most recommended and well tested wallet</div>
+            {metaMaskProviderFound && (
+              <ProviderElement
+                provider={metaMaskProviderFound}
+                key={metaMaskProviderFound.info.uuid}
+                setOpen={setOpen}
+              />
+            )}
+            {!metaMaskProviderFound && <InstallMetaMaskElement />}
+            {providers.find((provider) => provider.info.name !== 'MetaMask') && (
+              <>
+                <div className='font-semibold font-xl mt-8'>Other wallets we found installed</div>
+                {providers.map((provider) => (
+                  <div key={provider.info.uuid}>
+                    {provider.info.name !== 'MetaMask' && (
+                      <ProviderElement
+                        provider={provider}
+                        key={provider.info.uuid}
+                        setOpen={setOpen}
+                      />
+                    )}
+                  </div>
+                ))}
+              </>
+            )}
           </List>
         </motion.div>
       </DialogContent>
