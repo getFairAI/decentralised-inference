f80afe1e074a4aaabdf46a4b99c2e40c47d57f66
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2024-10-04 00:55:51 +0100

    feat: update deploy script

--- client/scripts/deploy.ts
+++ client/scripts/deploy.ts
@@ -16,7 +16,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-import Irys from '@irys/sdk';
+import ARx from '@permaweb/arx'
 import fs from 'fs';
 import { ANT, ArweaveSigner } from '@ar.io/sdk';
 
@@ -25,22 +25,18 @@ const main = async () => {
 
   const jwk = JSON.parse(fs.readFileSync(wallet).toString());
   
-  // NOTE: Depending on the version of JavaScript you use, you may need to use
-  // the commented out line below to create a new Bundlr object.
-  // const bundlr = new Bundlr("http://node1.bundlr.network", "arweave", jwk);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const irys = new Irys({ url: 'https://up.arweave.net', token: 'arweave', key: jwk });
+  const arx = new ARx({ url: 'https://up.arweave.net', token: 'arweave', key: jwk });
   
   // Get loaded balance in atomic units
-  const atomicBalance = await irys.getLoadedBalance();
+  const atomicBalance = await arx.getLoadedBalance();
   console.log(`node balance (atomic units) = ${atomicBalance}`);
   
   // Convert balance to an easier to read format
-  const convertedBalance = irys.utils.unitConverter(atomicBalance);
+  const convertedBalance = arx.utils.unitConverter(atomicBalance);
   console.log(`node balance (converted) = ${convertedBalance}`);
   
   // Print your wallet address
-  console.log(`wallet address = ${irys.address}`);
+  console.log(`wallet address = ${arx.address}`);
   const dist = './dist/';
 
   let forks = '';
@@ -51,24 +47,23 @@ const main = async () => {
   } catch (err) {
     // ignore
   }
-  
 
 
   const tags = [
-    { name: 'Title', value: 'Fair Protocol Marketplace' },
+    { name: 'Title', value: 'FairAI Marketplace' },
     { name: 'Description', value: 'Building the leading global decentralised marketplace for open-source AI' },
     { name: 'Published', value: String(Date.now()) },
-    { name: 'Page-Code', value: 'fair-protocol-marketplace' },
-    { name: 'Group-Id', value: 'fair-protocol-marketplace' },
+    { name: 'Page-Code', value: 'fairAI-marketplace' },
+    { name: 'Group-Id', value: 'fairAI-marketplace' },
     { name: 'App-Name', value: 'SmartWeaveContract' },
     { name: 'App-Version', value: '0.3.0' },
     { name: 'Contract-Src', value: 'h9v17KHV4SXwdW2-JHU6a23f6R0YtbXZJJht8LfP8QM'},
     { name: 'Logo', value: 'i4Hmf2yh-_TCA9-ypKeiEHhrIJlkhPyvZ_n4SOyj6BI'}, // logo txid
     {
       name: 'Init-State', value: JSON.stringify({
-        balances: { [`${irys.address}`]: 1 },
-        name: 'Fair Protocol Marketplace',
-        ticker: 'FPM',
+        balances: { [`${arx.address}`]: 1 },
+        name: 'FairAI Marketplace',
+        ticker: 'FAIAPP',
       })
     },
   ];
@@ -77,7 +72,7 @@ const main = async () => {
     tags.splice(4, 0, { name: 'Forks', value: forks });
   }
 
-  const response = await irys.uploadFolder(dist, {
+  const response = await arx.uploadFolder(dist, {
     manifestTags: tags, // tags to apply to the manifest
     indexFile: 'index.html', // optional index file (file the user will load when accessing the manifest)
     batchSize: 50, // number of items to upload at once
@@ -91,27 +86,26 @@ const main = async () => {
     return;
   }
 
-  const controllerWallet = './wallet-controller.json';
-
-  const controllerJWK = JSON.parse(fs.readFileSync(controllerWallet).toString());
   // in a node environment
-  const nodeSigner = new ArweaveSigner(controllerJWK);
+  const nodeSigner = new ArweaveSigner(jwk);
 
   const ant = ANT.init({
-    contractTxId: 'wAzmwCy7msdaNj9rhKRRIBlHFBx5n_yrMZQeo6bVrrQ', // fair.ar-io.dev contract
+    processId: 'wAzmwCy7msdaNj9rhKRRIBlHFBx5n_yrMZQeo6bVrrQ', // fair.ar-io.dev contract
     signer: nodeSigner,
   });
   const state = await ant.getState();
   console.log(state);
   console.log('previous Txid:', await ant.getRecords());
   
-  const subDomain = '@';
+  const undername = '@';
   const ttlSeconds = 900;
   const { id: txId } = await ant.setRecord({
-    subDomain,
+    undername,
     transactionId: response?.id,
     ttlSeconds,
-  });
+  }, { tags: [
+    { name: 'Unix-Time', value: String(Date.now() / 1000)},
+  ]});
 
   console.log(`Set record https://arweave.net/${txId}`);
 };
